91dea98da30b09db21381c221056d8d8134f39d4
Ducks404 <aghakoeswantoro@gmail.com>
2022-05-16 00:25:04 +0700

    Added a Test scene Added a Polar to Cartesian coord function Added a function to find hypotenuse given two sides

--- scene.py
+++ scene.py
@@ -1,1 +1,37 @@
 from manim import *
+import math as m
+
+def PoltoCar(r, theta):
+    theta = m.radians(theta)
+    x = round(r * m.cos(theta),2)
+    y = round(r * m.sin(theta), 2)
+    return({'x':x,'y':y})
+
+def hyp(a, b):
+    c = m.sqrt(a**2+b**2)
+    return(c)
+
+# circle.get_center
+
+class Test(Scene):
+    def construct(self):
+        circle = Circle(radius=0.5)
+        circ = Circle(radius=0.5)
+        circle.set_color(WHITE)
+        circ.set_color(WHITE)
+        # text = Text(str(circle.radius))
+        circle.shift([-1,0,0])
+        circ.shift([1,0,0])
+        # line = Line(start=[circle.get_x(), circle.get_y(), 0], end=[circ.get_x(), circ.get_y(), 0])
+        line = Line(start=[circle.get_x()+0.5, circle.get_y(), 0], end=[circ.get_x()-0.5, circ.get_y(), 0])
+
+        self.add(circle)
+        # text.shift(UP*2)
+        # self.add(text)
+        # self.play(ApplyMethod(circle.move_to, [1,0.5,0]), ApplyMethod(circ.move_to, [1,1,0]))
+        # des = PoltoCar(hyp(1,1), 180)
+        # self.play(ApplyMethod(circ.shift, [des['x'], des['y'], 0]))
+        # print(f'Line angle={line.get_angle()} \n Line slope={line.get_slope()}')
+
+        self.add(circle, circ, line)
+        # self.play(Create(line))
