0f97e59a30beb924ea1e40b41cd7fd81350372d4
Ducks404 <aghakoeswantoro@gmail.com>
2022-05-16 04:33:53 +0700

    Added connecting nodes with edges and updater functions to stick edges to nodes

--- node.py
+++ node.py
@@ -1,1 +1,61 @@
 from manim import *
+import math as m
+
+def PoltoCar(r, theta):
+    x = round(r * m.cos(theta),2)
+    y = round(r * m.sin(theta), 2)
+    return([x,y,0])
+
+def hyp(a, b):
+    c = m.sqrt(a**2+b**2)
+    return(c)
+
+def add2lists(l1, l2):
+    li = []
+    for d, i in enumerate(l1):
+        li.append(i-l2[d])
+    return li
+
+def stickLine(line, node, target):
+    # print(line.get_start_and_end(), node.node.get_center(), target.node.get_center())
+    nodeC = node.node.get_center()
+    targetC = target.node.get_center()
+    angle = angle_of_vector(nodeC - targetC)
+    # print(angle)
+    start = add2lists(nodeC, PoltoCar(node.radius, angle))
+    sign = int(angle<0)
+    if sign:
+        opp_angle = m.pi + angle
+    else:
+        opp_angle = -m.pi + angle
+    end = add2lists(targetC, PoltoCar(target.radius, opp_angle))
+    line.put_start_and_end_on(start, end)
+
+
+class Node():
+    def __init__(self, x, y, color, radius=1):
+        self.color = color
+        self.radius = radius
+        self.node = Circle(radius=radius, color=color).set_fill(BLACK, opacity=1.0).move_to([x,y,0])
+        self.edges=[]
+        
+    def show(self, scene):
+        # print(scene)
+        scene.play(FadeIn(self.node))
+        edge_animations = []
+        for i in self.edges:
+            edge_animations.append(FadeIn(i))
+        scene.play(AnimationGroup(*edge_animations))
+
+    def connect(self, target):
+        line = Line()
+        stickLine(line, self, target)
+        line.add_updater(lambda l: stickLine(l, self, target))
+        self.edges.append(line)
+
+'''
+Notes:
+if you want to connect then target should be the parent because line will show with self
+
+
+'''
--- scene.py
+++ scene.py
@@ -1,5 +1,6 @@
 from manim import *
 import math as m
+import node
 
 def PoltoCar(r, theta):
     theta = m.radians(theta)
@@ -23,7 +24,8 @@ class Test(Scene):
         circle.shift([-1,0,0])
         circ.shift([1,0,0])
         # line = Line(start=[circle.get_x(), circle.get_y(), 0], end=[circ.get_x(), circ.get_y(), 0])
-        line = Line(start=[circle.get_x()+0.5, circle.get_y(), 0], end=[circ.get_x()-0.5, circ.get_y(), 0])
+        # line = Line(start=[circle.get_x()+0.5, circle.get_y(), 0], end=[circ.get_x()-0.5, circ.get_y(), 0])
+        line = Line(start=[1,1,0], end=[-1,1,0])
 
         self.add(circle)
         # text.shift(UP*2)
@@ -31,7 +33,26 @@ class Test(Scene):
         # self.play(ApplyMethod(circle.move_to, [1,0.5,0]), ApplyMethod(circ.move_to, [1,1,0]))
         # des = PoltoCar(hyp(1,1), 180)
         # self.play(ApplyMethod(circ.shift, [des['x'], des['y'], 0]))
-        # print(f'Line angle={line.get_angle()} \n Line slope={line.get_slope()}')
+        print(f'Line angle={line.get_angle()} \nLine slope={line.get_slope()}')
+        # print(f'Line start={line.get_start_and_end()[0]} \nLine end={line.get_start_and_end()[1]}')
+        # line.put_start_and_end_on([-1,-1,0], line.get_end())
+        # print(f'Line start={line.get_start_and_end()[0]} \nLine end={line.get_start_and_end()[1]}')
 
         self.add(circle, circ, line)
         # self.play(Create(line))
+
+class TestNode(Scene):
+    def construct(self):
+        n = node.Node(0,0, WHITE, radius=0.5)
+        n1 = node.Node(0,0, WHITE, radius=0.5)
+        n2 = node.Node(0,-3, WHITE, radius=0.5)
+        # print(WHITE)
+        n.node.shift([-3,3,0])
+        n1.node.shift([0,0,0])
+        n1.connect(n)
+        n1.connect(n2)
+        n.show(self)
+        n2.show(self)
+        n1.show(self)
+        self.wait(1)
+        self.play(ApplyMethod(n1.node.shift, [1,1,0]), ApplyMethod(n.node.shift, [0,-1,0]))
